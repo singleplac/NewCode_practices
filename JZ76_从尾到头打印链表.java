35ddf9484f3afc8f347af7af80710e148abc8a54
singleplac <61055463+singleplac@users.noreply.github.com>
2021-12-02 18:24:23 +0800

    Update JZ76_从尾到头打印链表.java

--- JZ76_从尾到头打印链表.java
+++ JZ76_从尾到头打印链表.java
@@ -1,3 +1,4 @@
+//Method 1:
 import java.util.ArrayList;
 import java.util.Collections;
 public class Solution {
@@ -7,10 +8,28 @@ public class Solution {
             arr.add(listNode.val);
             listNode = listNode.next;
         }
-                  System.out.println("反转前排序: " + arr);            
+          //System.out.println("反转前排序: " + arr);            
           Collections.reverse(arr);
           System.out.println("反转后排序: " + arr);
         return arr;
 
+    }
+}
+
+//Method 2:
+import java.util.ArrayList;
+public class Solution {
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {       
+        while (listNode!=null){
+            arr.add(listNode.val);
+            listNode = listNode.next;
+        }
+       ArrayList<Integer> list = new ArrayList<>();
+        for (int i= arr.size()-1;i>=0;i--){
+            list.add(arr.get(i));
+        }
+        return list;
+        
     }
 }
