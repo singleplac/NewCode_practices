25cb40bd54eda4d97dafdc166e7b657496c792b5
singleplac <61055463+singleplac@users.noreply.github.com>
2021-12-02 20:51:45 +0800

    Add files via upload

---
+++ 最大公约数.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 求出a、b的最大公约数。
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public int gcd (int a, int b) {
+        // write code here
+       while (b%a !=0){
+           int tmp = a;
+           a = b%a;
+           b = tmp;
+       }
+        return a;
+    }
+}
