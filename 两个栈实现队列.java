d47c48444e33afa6ec6808961f365e7ca0acd0ad
singleplac <61055463+singleplac@users.noreply.github.com>
2021-12-02 19:05:10 +0800

    Add files via upload

---
+++ 两个栈实现队列.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack1.isEmpty() && stack2.isEmpty()){
+            return -1;
+        }
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()){
+            return -1;
+        }
+        return stack2.pop();
+    }
+}
