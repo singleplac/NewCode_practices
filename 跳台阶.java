ae648c36bc54e07569bf4c26b901674ff955992e
singleplac <61055463+singleplac@users.noreply.github.com>
2021-12-02 19:39:07 +0800

    Update 跳台阶.java

--- 跳台阶.java
+++ 跳台阶.java
@@ -1,6 +1,26 @@
 /**
 *简化版题目： 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
 *规律： method[n]=method[n-1]+method[n-2];
+*/
+public class Solution {
+    public int jumpFloor(int target) {
+        int times=0;
+        if (target==0){
+            return 0;
+        }else if(target==1){
+            return 1;
+        }
+        else if(target==2){
+             return 2;
+        }else{
+           
+             return jumpFloor(target-1) + jumpFloor(target-2);
+         
+        }
+  
+    }
+}
+/**
 *题目：一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
 *规律：method[n]=method[n-1]+method[n-2]+...+method[1]+method[1]+1;
 */
